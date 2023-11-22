0ae65112bdc43fd896d3858fbfbc2c577425a442
ArtemShushkanov <shuartale@yandex.ru>
2023-11-22 08:54:22 +0300

    Added Class Super student

--- src/main/kotlin/ru/gr106/main.kt
+++ src/main/kotlin/ru/gr106/main.kt
@@ -1,7 +1,14 @@
 package ru.gr106
 
+class SuperStudent(name:String, marks:Map<String,Int>) : AStudent{
+    override val name: String = name
+    var mmarks = mapOf<String,Int>()
+
+    init {
+        mmarks = marks
+    }
+}
 
 fun main() {
     val stds = mutableListOf<AStudent>()
-
-}
+    stds.add(SuperStudent("Jack", mapOf("chemistry" to 5,"Math" to 4)))}
