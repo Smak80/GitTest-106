132467ae062be168cfd031ac297b81a0822ff8ac
Sergey Makletsov <smaklets@kpfu.ru>
2023-11-22 09:11:37 +0300

    Вывод студентов в отдельной функции

--- src/main/kotlin/ru/gr106/main.kt
+++ src/main/kotlin/ru/gr106/main.kt
@@ -1,12 +1,15 @@
 package ru.gr106
 
+fun showStudents(sList: List<AStudent>){
+    sList.forEach {
+        println(it)
+    }
+}
 
 fun main() {
     val stds = mutableListOf<AStudent>()
     stds.add(Student("Иванов И. И."))
     stds.add(Student("Петров П. П."))
     stds.add(Student("Галимуллин Р. Ш."))
-    stds.forEach {
-        println(it)
-    }
+    showStudents(stds)
 }
