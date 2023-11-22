be8dc0e8e8f63625df0f457a61082902eb4c87e7
Аделя <laladesi@mail.ru>
2023-11-22 09:28:24 +0300

    dgreg

--- src/main/kotlin/ru/gr106/main.kt
+++ src/main/kotlin/ru/gr106/main.kt
@@ -3,8 +3,11 @@ package ru.gr106
 
 fun main() {
     val stds = mutableListOf<AStudent>()
+    val std3 = Student("Аня", 15, "111")
     val std1 = Student("Карина", 18, "555")
     val std2 = Student("Мария", 20, "543")
     stds.add(std1)
     stds.add(std2)
+    stds.add(std3)
+    std3.PrintStudentInfo(stds)
 }
