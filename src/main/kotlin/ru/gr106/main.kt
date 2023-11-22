23b4ed1007e3af418a7ca8103bd6bb253b33f953
Aleksey Suslov <lehass.2001@gmail.com>
2023-11-22 15:16:01 +0300

    add student

--- src/main/kotlin/ru/gr106/main.kt
+++ src/main/kotlin/ru/gr106/main.kt
@@ -4,6 +4,7 @@ package ru.gr106
 fun main() {
     val stds = mutableListOf<AStudent>()
     stds.add(Student("Ivan", 15u))
+    stds.add(Student("Dima", 21u))
     stds.add(Student("Vova", 20u))
     for (s in stds) {
         println(s)
