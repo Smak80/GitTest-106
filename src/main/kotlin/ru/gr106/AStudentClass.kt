2211e361d0775e9d73d8242c981bbd599029e369
greebear567 <nailkhakimov123@mail.ru>
2023-11-22 15:22:59 +0300

    2. Добавил отображение возраста и группы

--- src/main/kotlin/ru/gr106/AStudentClass.kt
+++ src/main/kotlin/ru/gr106/AStudentClass.kt
@@ -2,6 +2,6 @@ package ru.gr106
 
 class AStudentClass(override val name: String, override var age: UInt, override var group: String) : AStudent  {
     override fun toString(): String{
-        return name
+        return "ФИО:"+name+","+" Возраст:"+" "+age.toString()+","+" Группа:"+" "+group
     }
 }
