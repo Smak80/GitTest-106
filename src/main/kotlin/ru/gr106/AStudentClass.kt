b3faaf3bb9c2415ace377f118278a5984b7ac985
greebear567 <nailkhakimov123@mail.ru>
2023-11-22 14:47:15 +0300

    АПдейтнул, смерджил, поправил конфликты

--- src/main/kotlin/ru/gr106/AStudentClass.kt
+++ src/main/kotlin/ru/gr106/AStudentClass.kt
@@ -1,6 +1,6 @@
 package ru.gr106
 
-class AStudentClass(override val name: String) : AStudent  {
+class AStudentClass(override val name: String, override var age: UInt, override var group: String) : AStudent  {
     override fun toString(): String{
         return name
     }
--- src/main/kotlin/ru/gr106/main.kt
+++ src/main/kotlin/ru/gr106/main.kt
@@ -5,8 +5,8 @@ fun display_information(some: List<AStudent>){
 }
 fun main() {
     val stds = mutableListOf<AStudent>()
-    stds.add(AStudentClass("Некто Н.Н."))
-    stds.add(AStudentClass("Второй Н.Н"))
+    stds.add(AStudentClass("Некто Н.Н.", 12u, "05-004"))
+    stds.add(AStudentClass("Второй Н.Н", 16u, "05-005"))
     stds.forEach{
         println(it)
     }
