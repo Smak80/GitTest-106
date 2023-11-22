f2a406b57308baf997517acf7e59a49811a2e96c
ArtemShushkanov <shuartale@yandex.ru>
2023-11-22 09:04:36 +0300

    Added printing function

--- src/main/kotlin/ru/gr106/main.kt
+++ src/main/kotlin/ru/gr106/main.kt
@@ -7,10 +7,22 @@ class SuperStudent(name:String, marks:Map<String,Int>) : AStudent{
     init {
         mmarks = marks
     }
+
+    override fun toString(): String {
+        return "This student is called $name"
+    }
+}
+
+fun printStudents(students:List<AStudent>){
+    for (student in students){
+        println(student)
+    }
+
 }
 
 fun main() {
     val stds = mutableListOf<AStudent>()
     stds.add(SuperStudent("Jack", mapOf("chemistry" to 5,"Math" to 4)))
     stds.add(SuperStudent("Robin", mapOf("PE" to 5,"Literature" to 5)))
+    printStudents(stds)
 }
