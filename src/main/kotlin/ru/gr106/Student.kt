da98a7cbd8a7bf76f0e77b6e81120fbc473e9aad
Аделя <laladesi@mail.ru>
2023-11-22 15:14:21 +0300

    Init

--- src/main/kotlin/ru/gr106/Student.kt
+++ src/main/kotlin/ru/gr106/Student.kt
@@ -2,7 +2,8 @@ package ru.gr106
 
 class Student(Name: String, Age: UInt, Group: String) : AStudent{
     override val name = Name
-    override val age = Age
+    override var age = Age
+    override var group = Group
     fun PrintStudentsInfo(students: List<AStudent>){
         students.forEach { it -> println("Имя студента:" + " " + it.name + "\n") }
     }
