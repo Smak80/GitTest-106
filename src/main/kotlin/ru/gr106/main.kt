a0ce6cf1303ec23ba11965b315bd17a429346889
Sergey Makletsov <smaklets@kpfu.ru>
2023-11-22 09:58:13 +0300

    Добавление студента

--- src/main/kotlin/ru/gr106/main.kt
+++ src/main/kotlin/ru/gr106/main.kt
@@ -9,6 +9,8 @@ fun showStudents(sList: List<AStudent>){
 fun main() {
     val stds = mutableListOf<AStudent>()
     stds.add(Student("Иванов И. И."))
+    stds.add(Student("Петров П. П."))
+    stds.add(Student("Галимуллин Р. Ш."))
     stds.add(Student("Сидоров С. В."))
     showStudents(stds)
 }
