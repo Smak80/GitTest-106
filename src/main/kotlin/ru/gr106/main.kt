62e909cb0dd3a148e52ab526d64e058337e0749e
ArtemShushkanov <shuartale@yandex.ru>
2023-11-22 09:39:50 +0300

    Deleted one student

--- src/main/kotlin/ru/gr106/main.kt
+++ src/main/kotlin/ru/gr106/main.kt
@@ -1,5 +1,7 @@
 package ru.gr106
 
+
+
 fun printStudents(students:List<AStudent>){
     for (student in students){
         println(student)
@@ -9,6 +11,5 @@ fun printStudents(students:List<AStudent>){
 fun main() {
     val stds = mutableListOf<AStudent>()
     stds.add(SuperStudent("Jack", mapOf("chemistry" to 5,"Math" to 4)))
-    stds.add(SuperStudent("Robin", mapOf("PE" to 5,"Literature" to 5)))
     printStudents(stds)
 }
