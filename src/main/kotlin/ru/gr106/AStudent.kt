f69fb4abbbb7e381a117d999b457e7567ab2382e
Аделя <laladesi@mail.ru>
2023-11-22 09:17:37 +0300

    grer

--- src/main/kotlin/ru/gr106/AStudent.kt
+++ src/main/kotlin/ru/gr106/AStudent.kt
@@ -2,4 +2,7 @@ package ru.gr106
 
 interface AStudent {
     val name: String
+    fun Print(students: List<AStudent>){
+        print(0)
+    }
 }
--- src/main/kotlin/ru/gr106/main.kt
+++ src/main/kotlin/ru/gr106/main.kt
@@ -3,5 +3,8 @@ package ru.gr106
 
 fun main() {
     val stds = mutableListOf<AStudent>()
-
+    val std1 = Student("Карина", 18, "555")
+    val std2 = Student("Мария", 20, "543")
+    stds.add(std1)
+    stds.add(std2)
 }
