32beae38eeeea328c9dca48663a37bc72492e49e
Aleksey Suslov <lehass.2001@gmail.com>
2023-11-22 15:21:05 +0300

    объединение проектов

--- src/main/kotlin/ru/gr106/Student.kt
+++ src/main/kotlin/ru/gr106/Student.kt
@@ -1,6 +1,10 @@
 package ru.gr106
 
-class Student(override val name: String, override var age: UInt) : AStudent {
+class Student(
+    override val name: String,
+    override var age: UInt,
+    override var group: String = "05-106",
+) : AStudent {
     override fun toString(): String {
         return "$name $age"
     }
